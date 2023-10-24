f564e2ba48c703e425f05ce88aa9a674bedb29c9
abiodun-adedapo <abioluwapelumi@gmail.com>
2023-10-24 14:29:00 +0100

    all done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  WhatGPT3,
+  Header,
+  Features,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Readme.md
@@ -0,0 +1,1 @@
+This is gpt3 created by my 
